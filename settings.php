cab1a385311bc3ac315bf15f5e4c8e44ae1d7926
Romanos <54671506+Sroot-R@users.noreply.github.com>
2021-05-05 16:17:10 +0200

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,9 +1,9 @@
 <?php
 $settings = [
-    'host' => 'localhost',
-    'dbname' => 'dictionary',
-    'username' => 'root',
-    'password' => '',
+    'host' => 'hostname', //type your hostname
+    'dbname' => 'dbname', //type your dbname
+    'username' => 'username', //type your username
+    'password' => 'password', //type your passowrd
 ];
 
 define('ROOT', 'http://'.$_SERVER['HTTP_HOST'].'/current_version_of_projects/dict/procedural/project_root/');
